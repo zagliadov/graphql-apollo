d556f1151e9858e3f751bd7433d9753e9e24614b
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-01-24 20:12:35 +0200

    first try query push from client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
-import {FC} from "react";
+import { FC } from "react";
+import { useQuery, gql } from "@apollo/client";
+
+
+const GET_ALL_USERS = gql`
+  query {
+    getAllUsers {
+      name, username, email
+    }
+  }
+`;
 
 export const App: FC = () => {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
 
+  const {error, data, loading} = useQuery(GET_ALL_USERS);
 
+  console.log(data.getAllUsers);
+
+  return <div className="App"></div>;
+};
