aa01bbef104d686be88814aea43e6357f950b6fe
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-01-24 20:08:41 +0200

    @apollo/client init ApolloProvider added

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {App} from './App';
+import { App } from "./App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-
