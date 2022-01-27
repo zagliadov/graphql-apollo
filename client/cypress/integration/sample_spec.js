01a2c9f6ebd08b4441a30ccd55bea1ee4f7a1cd1
Daniil Zahliadov <portal.sid.grand2@gmail.com>
2022-01-27 15:00:20 +0200

    tests is good

--- client/cypress/integration/sample_spec.js
+++ client/cypress/integration/sample_spec.js
@@ -1,5 +1,19 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
+/* eslint-disable no-undef */
+describe("My First Test", () => {
+  it('finds the content "type"', () => {
+    // cy.visit('https://example.cypress.io')
+
+    cy.visit("http://localhost:3000/");
+    cy.contains("Leanne Graham").click();
+    cy.url().should("include", "/user/1");
+    cy.visit("http://localhost:3000/");
+    cy.get(".sc-eCImPb").click();
+
+    cy.get("input").first()
+      .type("Jane Dow")
+      .should("have.value", "Jane Dow")
+      .next().type('Janee').should('have.value', 'Janee')
+      .next().type('Jane@email.com').should('have.value', 'Jane@email.com')
+      .next().click()
+  });
+});
